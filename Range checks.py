e8c8f4027f558146a2ab3fee33b89097d83f0575
Praveen12345666 <praveenramavath551@gmail.com>
2025-01-15 13:30:09 +0530

    here we execute the Range checks

---
+++ Range checks.py
@@ -0,0 +1,7 @@
+values = [-15, -5, 0, 12, 8]
+
+for value in values:
+    if value < -10 or value > 10:
+        print(f"{value} is either less than -10 or greater than 10.")
+    else:
+        print(f"{value} is not less than -10 or greaterthan10.")
