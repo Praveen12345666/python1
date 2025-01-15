36f55144c37209edd61d868cfe90d4582dc5aa87
Praveen12345666 <praveenramavath551@gmail.com>
2025-01-15 13:27:03 +0530

    here we execute the even and positive number

---
+++ Even & Positive Number.py
@@ -0,0 +1,7 @@
+numbers = [14, -4, 7, 18, -10]  # Sample list of numbers
+
+for number in numbers:
+    if number > 0 and number % 2 == 0:
+        print(f"{number} is both even and positive.")
+    else:
+        print(f"{number} is not both evenandpositive.")
